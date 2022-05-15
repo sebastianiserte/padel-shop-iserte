f95554af0beb4b32bf1a007da2f0afa63b77c3ca
Sebastian I <sebastian.iserte@gmail.com>
2022-05-15 10:28:49 -0500

    Desafío: Item Collection

--- src/Components/ItemDetailContainer.jsx
+++ src/Components/ItemDetailContainer.jsx
@@ -2,41 +2,52 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail"
 import { GridLoader } from "react-spinners";
+import { doc,getDoc,getFirestore} from "firebase/firestore"
 
 function ItemDetailContainer() {
   
   /** variables y states*/
   const {id} = useParams();
   const [data, setData] = useState({});
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
   //Hook effect on mount con delay
   useEffect(()=>{
     
     //Cambio los estador para solo mostrar el loader
-    setLoading(true);
+    // setLoading(true);
     setData([]);
 
     //Recupero datos del archivo json
-    const BackEndData = fetch("../../data/productos.json", {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response=> response.json())
-    .then(prod=>prod.find((i) => i.id === id));
+    // const BackEndData = fetch("../../data/productos.json", {
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json'
+    //   }
+    // })
+    // .then(response=> response.json())
+    // .then(prod=>prod.find((i) => i.id === id));
 
-    //Declaro el Promise con Async 2s
-    const getItem = new Promise((res,rej)=>{
-      setTimeout(()=>res(BackEndData),500);
-    });
+    // //Declaro el Promise con Async 2s
+    // const getItem = new Promise((res,rej)=>{
+    //   setTimeout(()=>res(BackEndData),500);
+    // });
     
     //Resuelvo el Promise con estados nuevos
-    getItem.then(res=>{
-      setData(res);
-      setLoading(false);
-    });
+    // getItem.then(res=>{
+    //   setData(res);
+    //   setLoading(false);
+    // });
+
+    const db = getFirestore()
+    const itemRef = doc(db,"items",id)
+    getDoc(itemRef).then((snapshot)=>{
+      if(snapshot.exists()){
+        setData({id:snapshot.id, ...snapshot.data() });
+        // setLoading(false);
+      }
+    })
+
       
   },[id]);
   
@@ -45,8 +56,8 @@ function ItemDetailContainer() {
 
      <div className="w-10/12 mx-auto min-h-[72vh]">
       {/* pregutno por el id para saber que si tengo los datos para renderizar, sino no renderizo nada (no supe que era mejor si ""/null/undefined)*/}
-      {data.id ? <ItemDetail detail={data}/>:undefined}
-      <GridLoader color={'#394e6a'} loading={loading} size={15} css="display:flex;flex-wrap: wrap;position:fixed;top:50%;left: 48.3%;"/>
+      {data.id ? <ItemDetail detail={data}/>:<GridLoader color={'#394e6a'} loading={true} size={15} css="display:flex;flex-wrap: wrap;position:fixed;top:50%;left: 48.3%;"/>}
+      
     </div>
 
   )
--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GridLoader } from "react-spinners";
 import ItemList from "./ItemList";
-//import {collection, doc,getDoc,getDocs,getFirestore} from "firebase/firestore"
+import {collection, getDocs,getFirestore} from "firebase/firestore"
 
 
 function ItemListContainer({greeting}) {
@@ -10,7 +10,7 @@ function ItemListContainer({greeting}) {
   /** variables */
   const {id} = useParams();
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
   useEffect(()=>{
     /**recupero el archivo con los datos de produtos 
@@ -18,58 +18,60 @@ function ItemListContainer({greeting}) {
      * por ejemplo para el home "/" -> ./data/productos.json ok
      *    categorias "/category/id" -> /category/id/data/productos.json no ok :S
     */
-    let ruta="data/productos.json";
-    id===undefined?ruta= ("./").concat(ruta):ruta= ("../../").concat(ruta);
+    // let ruta="data/productos.json";
+    // id===undefined?ruta= ("./").concat(ruta):ruta= ("../../").concat(ruta);
 
     //Cambio los estador para solo mostrar el loader
-    setLoading(true);
-    setData([]);
+    // setLoading(true);
+    //setData([]);
 
     //Recupero datos del archivo json
-    const BackEndData = fetch(ruta, {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(response=> response.json())
-      .then(prod=>{
-        if (id===undefined){
-          return prod;
-        } else {
-          return prod.filter((p)=>p.categoria === id);
-        }
-      });
-
-    //Promise con Async
-    const promise = new Promise((res,rej)=>{
-        setTimeout(()=>res(BackEndData),500);
-    });
-
-    //Resuelvo el Promise con estados nuevos
-    promise.then(res=>{
-      setData(res);
-      setLoading(false);
-    });
-
-    // const db = getFirestore();
-    // const itemColection = collection(db,"items")
-    // getDocs(itemColection).then((snapshot)=>{
-        
-    //     setLoading(false);
-    //     setData(snapshot.docs.map((doc)=>({
-    //       id: doc.id, ...doc.data()
-    //     })))
-        
+    // const BackEndData = fetch(ruta, {
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json'
+    //   }
     // })
+    //   .then(response=> response.json())
+    //   .then(prod=>{
+    //     if (id===undefined){
+    //       return prod;
+    //     } else {
+    //       return prod.filter((p)=>p.categoria === id);
+    //     }
+    //   });
+
+    // //Promise con Async
+    // const promise = new Promise((res,rej)=>{
+    //     setTimeout(()=>res(BackEndData),500);
+    // });
+
+    // //Resuelvo el Promise con estados nuevos
+    // promise.then(res=>{
+    //   setData(res);
+    //   setLoading(false);
+    // });
+
+    setData([]);
+    const db = getFirestore();
+    const itemColection = collection(db,"items")
+    getDocs(itemColection).then((snapshot)=>{
+        
+        // setLoading(false);
+        setData(snapshot.docs.map((doc)=>({
+          id: doc.id, ...doc.data()
+        })))
+        
+    })
 
   },[id]);
   
   return (
       <div className="w-10/12 mx-auto min-h-[72vh]">
         <h1 className="text-4xl my-8" >Hola {greeting}</h1>
-        <ItemList items={data}/>
-        <GridLoader color={'#394e6a'} loading={loading} size={15} css="display:flex;flex-wrap: wrap;position:fixed;top:50%;left: 48.3%;"/>
+        {data.length===0?
+          <GridLoader color={'#394e6a'} loading={true} size={15} css="display:flex;flex-wrap: wrap;position:fixed;top:50%;left: 48.3%;"/>
+          :<ItemList items={data}/>}
       </div>
   )
 }
